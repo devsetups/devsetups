a5aa0f921e986e3f22d83d585de6eac532b4ad14
Daniel Castro <dancasttro@gmail.com>
2019-11-12 03:29:29 -0300

    commit initial

---
+++ src/components/Layout/index.js
@@ -0,0 +1,19 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+import Header from "../Header"
+import "../layout.css"
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
